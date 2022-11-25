11c940556f4814e4e898ac709a7136bc900098da
Laion Pereira <kingslaion@gmail.com>
2022-11-25 14:06:28 -0300

    exercicio de arrays

---
+++ exercicio1/script.js
@@ -0,0 +1,11 @@
+let number = [5, 8, 3, 9];
+let arr = ['iphone', 'samsung', 'motorola'];
+let arrNovo = [3, 8, 'edson', 'lucas', true, 'renan', false];
+
+console.log(number)
+console.log(arr)
+console.log(arrNovo)
+
+
+
+
---
+++ exercicio3/script.js
@@ -0,0 +1,34 @@
+let nomeDoArrayOriginalCopia = ['jose', 5, 'paula'];
+
+console.log(nomeDoArrayOriginalCopia);
+
+
+nomeDoArrayOriginalCopia.unshift('number');
+console.log(nomeDoArrayOriginalCopia);
+
+
+let nomeDoArrayOriginalCopia1 = ['paulo', 6, 'pedro']
+
+console.log(nomeDoArrayOriginalCopia1);
+
+
+nomeDoArrayOriginalCopia1.pop('pedro');
+console.log(nomeDoArrayOriginalCopia1);
+
+
+
+let nomeDoArrayOriginalCopia2 = ['edson', 'chico', 3];
+
+console.log(nomeDoArrayOriginalCopia2);
+
+nomeDoArrayOriginalCopia = nomeDoArrayOriginalCopia2.splice(1,1)
+console.log(nomeDoArrayOriginalCopia2)
+
+
+
+
+
+    
+
+
+
